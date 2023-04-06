4a6b4b9c031a2a1664333b4a13d8eb4452af3d97
Don Carderms <donphilocardermssouffrant@gmail.com>
2023-04-06 09:31:11 -0300

    refactor: remove comments

--- backend/materiais/views.py
+++ backend/materiais/views.py
@@ -39,35 +39,6 @@ def apiOverview(request):
 
 
 
-
-
-# @csrf_exempt
-# def departmentApi(request,id=0):
-#     if request.method=='GET':
-#         departments = Departments.objects.all()
-#         departments_serializer=DepartmentSerializer(departments,many=True)
-#         return JsonResponse(departments_serializer.data,safe=False)
-#     elif request.method=='POST':
-#         department_data=JSONParser().parse(request)
-#         departments_serializer=DepartmentSerializer(data=department_data)
-#         if departments_serializer.is_valid():
-#             departments_serializer.save()
-#             return JsonResponse("Added Successfully",safe=False)
-#         return JsonResponse("Failed to Add",safe=False)
-#     elif request.method=='PUT':
-#         department_data=JSONParser().parse(request)
-#         department=Departments.objects.get(DepartmentId=department_data['DepartmentId'])
-#         departments_serializer=DepartmentSerializer(department,data=department_data)
-#         if departments_serializer.is_valid():
-#             departments_serializer.save()
-#             return JsonResponse("Updated Successfully",safe=False)
-#         return JsonResponse("Failed to Update")
-#     elif request.method=='DELETE':
-#         department=Departments.objects.get(DepartmentId=id)
-#         department.delete()
-#         return JsonResponse("Deleted Successfully",safe=False)
-
-
 @api_view(['GET'])
 def userList(request):
 	users = User.objects.all().order_by('-id')
