dedc665d94976a735e4569b42e2547b169a6ca71
Don Carderms <donphilocardermssouffrant@gmail.com>
2023-04-06 09:22:56 -0300

    feat : add containers docker and finalize a api

--- backend/materiais/admin.py
+++ backend/materiais/admin.py
@@ -4,17 +4,19 @@ from django.contrib import admin
 from.models import *
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email')
-    search_fields = ('username', 'email')
-    list_filter = ('username', 'email')
+    list_display = ('username', 'email', 'active')
+    search_fields = ('username', 'email', 'active')
+    list_filter = ('username', 'email', 'active')
 
 class MaterialAdmin(admin.ModelAdmin):
     list_display = ('name', 'user', 'active', 'created_at', 'updated_at')
     search_fields = ('title', 'description')
+    list_filter = ('user', 'active')
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'material', 'comment', 'created_at', 'updated_at')
+    list_display = ('comment','user', 'material', 'created_at', 'updated_at')
     search_fields = ('comment',)
+    list_filter = ('user', 'material')
 
 admin.site.register(User,UserAdmin)
 admin.site.register(Material,MaterialAdmin)
--- backend/materiais/views.py
+++ backend/materiais/views.py
@@ -1,9 +1,11 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import UserSerializer, MaterialSerializer, CommentSerializer
+from rest_framework.parsers import JSONParser
 
+from .serializers import UserSerializer, MaterialSerializer, CommentSerializer
 from .models import Material, User, Comment
 # Create your views here.
 
@@ -14,26 +16,57 @@ def apiOverview(request):
 			'List':'/user-list/',
 			'Detail View':'/user-detail/<str:pk>/',
 			'Create':'/new-user/',
-			'Update':'/user-update/<str:pk>/',
-			'Delete':'/user-delete/<str:pk>/',
+			'Update':'/update-user/<str:pk>/',
+			'Delete':'/delete-user/<str:pk>/',
 		},
 		'material': {		
 			'List':'/material-list/',
 			'Detail View':'/material-detail/<str:pk>/',
 			'Create':'/material-create/',
-			'Update':'/material-update/<str:pk>/',
-			'Delete':'/material-delete/<str:pk>/',
+			'Update':'/update-material/<str:pk>/',
+			'Delete':'/delete-material/<str:pk>/',
 		},
 		'comment': {		
 			'List':'/comment-list/',
 			'Detail View':'/comment-detail/<str:pk>/',
 			'Create':'/comment-create/',
-			'Update':'/comment-update/<str:pk>/',
-			'Delete':'/comment-delete/<str:pk>/',
+			'Update':'/update-comment/<str:pk>/',
+			'Delete':'/delete-comment/<str:pk>/',
 		},
 		}
 
 	return Response(api_urls)
+
+
+
+
+
+# @csrf_exempt
+# def departmentApi(request,id=0):
+#     if request.method=='GET':
+#         departments = Departments.objects.all()
+#         departments_serializer=DepartmentSerializer(departments,many=True)
+#         return JsonResponse(departments_serializer.data,safe=False)
+#     elif request.method=='POST':
+#         department_data=JSONParser().parse(request)
+#         departments_serializer=DepartmentSerializer(data=department_data)
+#         if departments_serializer.is_valid():
+#             departments_serializer.save()
+#             return JsonResponse("Added Successfully",safe=False)
+#         return JsonResponse("Failed to Add",safe=False)
+#     elif request.method=='PUT':
+#         department_data=JSONParser().parse(request)
+#         department=Departments.objects.get(DepartmentId=department_data['DepartmentId'])
+#         departments_serializer=DepartmentSerializer(department,data=department_data)
+#         if departments_serializer.is_valid():
+#             departments_serializer.save()
+#             return JsonResponse("Updated Successfully",safe=False)
+#         return JsonResponse("Failed to Update")
+#     elif request.method=='DELETE':
+#         department=Departments.objects.get(DepartmentId=id)
+#         department.delete()
+#         return JsonResponse("Deleted Successfully",safe=False)
+
 
 @api_view(['GET'])
 def userList(request):
@@ -49,10 +82,98 @@ def userDetail(request, pk):
 
 @api_view(['POST'])
 def newUser(request):
-	print(f'json: {request.data}')
-	serializer = User(data=request.data)
+	serializer = UserSerializer(data=request.data)
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+@api_view(['PUT'])
+def updateUser(request, pk):
+    user = User.objects.get(id=pk)
+    serializer = UserSerializer(instance=user, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteUser(request, pk):
+    user = User.objects.get(id=pk)
+    user.delete()
+    return Response("User deleted successfully")
+
+
+
+
+@api_view(['GET'])
+def materialList(request):
+	materiais = Material.objects.all().order_by('-id')
+	serializer = MaterialSerializer(materiais,many=True)
+	return Response(serializer.data)
+
+@api_view(['GET'])
+def materialDetail(request, pk):
+	material = Material.objects.get(id=pk)
+	serializer = MaterialSerializer(material, many=False)
+	return Response(serializer.data)
+
+@api_view(['POST'])
+def createMaterial(request):
+	serializer = MaterialSerializer(data=request.data)
 
 	if serializer.is_valid():
 		serializer.save()
 
 	return Response(serializer.data)
+
+@api_view(['PUT'])
+def updateMaterial(request, pk):
+    material = Material.objects.get(id=pk)
+    serializer = MaterialSerializer(instance=material, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteMaterial(request, pk):
+    material = Material.objects.get(id=pk)
+    material.delete()
+    return Response("material deleted successfully")
+
+
+
+
+@api_view(['GET'])
+def commentList(request):
+	comments = Comment.objects.all().order_by('-id')
+	serializer = CommentSerializer(comments,many=True)
+	return Response(serializer.data)
+
+@api_view(['GET'])
+def commentDetail(request, pk):
+	comment = Comment.objects.get(id=pk)
+	serializer = CommentSerializer(comment, many=False)
+	return Response(serializer.data)
+
+@api_view(['POST'])
+def createComment(request):
+	serializer = CommentSerializer(data=request.data)
+
+	if serializer.is_valid():
+		serializer.save()
+
+	return Response(serializer.data)
+
+@api_view(['PUT'])
+def updateComment(request, pk):
+    comment = Comment.objects.get(id=pk)
+    serializer = CommentSerializer(instance=comment, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def deleteComment(request, pk):
+    comment = Comment.objects.get(id=pk)
+    comment.delete()
+    return Response("comment deleted successfully")
